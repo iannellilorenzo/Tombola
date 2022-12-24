4a317c1540bfea1b26175e5e3f250748a5c6bce5
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2022-12-24 17:06:05 +0100

    Agg: Interfaccia grafica To-Do: Generazione cartelle e matrici

--- Tombola/Program.cs
+++ Tombola/Program.cs
@@ -4,38 +4,61 @@ class Program
 {
     static void Main()
     {
-        // dichiarazione
-        int righe = 3, colonne = 5, min = 1, max = 90, xT = 20, yT = 10, xC1 = 20, yC1 = 20;
-        int[,] tabellone = new int[10, 10];
-        int[,] cartella1 = new int[righe, colonne];
-        int[,] cartella2 = new int[righe, colonne];
+        // dichiarazione variabili
+        int righeCart = 3, colonneCart = 5, righeTab = 8, colonneTab = 10;
+        int min = 1, max = 90;
+        int xTab = 50, yTab = 1, xCart1 = 15, yCart1 = 11, xCart2 = 85, yCart2 = 11;
+
+        // dichiarazione matrici
+        int[,] tabellone = new int[righeTab, colonneTab];
+        int[,] cartella1 = new int[righeCart, colonneCart];
+        int[,] cartella2 = new int[righeCart, colonneCart];
+
+        // random
         Random random = new Random();
 
-        // set del cursore tabellone
-
-        for (int i = 0; i < 10; i++)
+        // stampa tabellone
+        Console.SetCursorPosition(55, 0);
+        Console.WriteLine("Tabellone");
+        for (int i = 0; i < righeTab; i++)
         {
-            Console.SetCursorPosition(xT, yT);
             Console.WriteLine();
-            for (int j = 0; j < 8; j++)
+            Console.SetCursorPosition(xTab, yTab);
+            for (int j = 0; j < colonneTab; j++)
             {
-                Console.Write(tabellone[i, j]);
+                Console.Write(tabellone[i, j].ToString() + " ");
             }
-            xT++;
-            yT++;
+            yTab++;
         }
 
-        // generazione cartella1
-        for (int i = 0; i < righe; i++)
+        // stampa cartella1
+        Console.SetCursorPosition(14, 10);
+        Console.WriteLine("Cartella giocatore 1");
+        for (int i = 0; i < righeCart; i++)
         {
-            Console.SetCursorPosition(xC1, yC1);
             Console.WriteLine();
-            for (int j = 0; j < colonne; j++)
+            Console.SetCursorPosition(xCart1, yCart1);
+            for (int j = 0; j < colonneCart; j++)
             {
                 cartella1[i, j] = random.Next(min, max + 1);
                 Console.Write(cartella1[i, j].ToString("D2") + "  ");
             }
-            xC1++; yC1++;
+            yCart1++;
+        }
+
+        // stampa cartella2
+        Console.SetCursorPosition(84, 10);
+        Console.WriteLine("Cartella giocatore 2");
+        for (int i = 0; i < righeCart; i++)
+        {
+            Console.WriteLine();
+            Console.SetCursorPosition(xCart2, yCart2);
+            for (int j = 0; j < colonneCart; j++)
+            {
+                cartella2[i, j] = random.Next(min, max + 1);
+                Console.Write(cartella2[i, j].ToString("D2") + "  ");
+            }
+            yCart2++;
         }
 
         Console.ReadKey();
